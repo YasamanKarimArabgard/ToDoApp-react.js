adf2b78a1b9ee8c3dc034a7273158dec88a89c02
jasminearabgard <yasaman.arabgard@outlook.com>
2022-02-08 14:00:07 +0330

    install some packages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import ToDoApp from "./Components/ToDoApp/ToDoApp"
 
 const App = () => {
 
-  
-
   return (
-   <></>
+    <div className="container-fluid d-flex align-items-center flex-column bg-light">
+      <h1 className="pt-4">Todo app with Jasmine</h1>
+      <ToDoApp />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 import "./index.css"
 import App from './App';
 
