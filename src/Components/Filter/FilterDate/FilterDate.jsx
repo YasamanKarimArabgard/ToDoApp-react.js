15d53afce3fc329d6ba8d0b47b1178189193b452
jasminearabgard <yasaman.arabgard@outlook.com>
2022-02-12 14:09:34 +0330

    filter by date com

--- src/Components/Filter/FilterDate/FilterDate.jsx
+++ src/Components/Filter/FilterDate/FilterDate.jsx
@@ -13,7 +13,7 @@ const FilterDate = ({ selectedDateOption , onChange }) => {
 
     return (
         <div className='col-6 col-sm-12 mt-1 d-flex align-items-center justify-content-between'>
-        <p className='text-muted'>Date : </p>
+        <p className='text-info'>Date : </p>
             <Select options={options}
                 value={selectedDateOption}
                 onChange={onChange} />
