b69bd726616a141a0a2193fc2d7ac011a613948b
jasminearabgard <yasaman.arabgard@outlook.com>
2022-02-12 14:16:08 +0330

    some cahnges in ui

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
 
   return (
     <div className="container-fluid d-flex align-items-center flex-column bg-light">
-      <h1 className="pt-4">Todo app with Jasmine</h1>
+      <h1 className="pt-4 font-weight-bold">ToDo App by Jasmine</h1>
       <ToDoApp />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css"
 import App from './App';
 
