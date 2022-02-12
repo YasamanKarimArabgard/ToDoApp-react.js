5936120eb89e573c2ab34ce53e0dd7894780d5af
jasminearabgard <yasaman.arabgard@outlook.com>
2022-02-12 14:05:59 +0330

    filter by date

---
+++ src/Components/Filter/FilterDate/FilterDate.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Select from 'react-select';
+
+
+const options = [
+    { value: 'Default', label: 'Default' },
+    { value: 'Latest', label: 'Latest' },
+    { value: 'Earliest', label: 'Earliest' }
+
+]
+
+const FilterDate = ({ selectedDateOption , onChange }) => {
+
+    return (
+        <div className='col-6 col-sm-12 mt-1 d-flex align-items-center justify-content-between'>
+        <p className='text-muted'>Date : </p>
+            <Select options={options}
+                value={selectedDateOption}
+                onChange={onChange} />
+        </div>
+    );
+};
+
+export default FilterDate;
