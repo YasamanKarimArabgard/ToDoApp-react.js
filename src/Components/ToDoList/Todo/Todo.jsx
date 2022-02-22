1b70c6a9926804f0d9daef2ab60b4806dd2e75ae
jasminearabgard <yasaman.arabgard@outlook.com>
2022-02-22 18:57:07 +0330

    add space

--- src/Components/ToDoList/Todo/Todo.jsx
+++ src/Components/ToDoList/Todo/Todo.jsx
@@ -7,9 +7,9 @@ const Todo = ({ todo, onEdit }) => {
     const dispatch = useTodosActions();
 
     return (
-        <li key={todo.id} className={`list-group-item col-12 d-flex justify-content-between align-items-center mb-1 border ${todo.isCompleted ? 'border border-secondary' : ''}`}>
+        <li key={todo.id} className={`list-group-item col-12 d-flex justify-content-between align-items-center mb-1 border p-2${todo.isCompleted ? 'border border-secondary' : ''}`}>
             <div className='col-3'>
-                <p onClick={() => dispatch({ type: 'completeTodo', id: todo.id })} className={`todo-text d-flex flex-wrap overflow-hidden ${todo.isCompleted ? 'text-decoration-line-through text-muted' : ''}`}>{todo.text}</p>
+                <p onClick={() => dispatch({ type: 'completeTodo', id: todo.id })} className={`todo-text d-flex flex-wrap overflow-hidden p-1 ${todo.isCompleted ? 'text-decoration-line-through text-muted' : ''}`}>{todo.text}</p>
             </div>
             <div className='col-6 d-flex flex-column h-100 justify-content-start px-1'>
                 {todo.updated ?
